0dbcfaca441e2802c35a769606a19b54809b9e14
div-mittal <113824753+div-mittal@users.noreply.github.com>
2024-11-24 18:20:59 +0530

    debugging

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
     try {
-        console.log(first, process.env.MONGODB_URI, DB_NAME);
+        console.log(process.env.MONGODB_URI, DB_NAME);
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         console.log(`MongoDB connected: ${connectionInstance.connection.host}`);
         
